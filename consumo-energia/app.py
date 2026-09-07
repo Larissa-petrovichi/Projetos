b435d03f2acfcd781fed3033f7e9e65d2aa5c97e
Larissa <317431144+Larissa-petrovichi@users.noreply.github.com.>
2026-09-07 12:46:51 -0300

    Style: Ajustar espaçamento

--- consumo-energia/app.py
+++ consumo-energia/app.py
@@ -1,8 +1,19 @@
-nome = input("nome do aparelho: ")
+# Dados do aparelho
+nome = input("Nome do aparelho: ")
+
+# Entrada de dados do uso do aparelho
 potência = float(input("Potência do aparelho (em Watts): "))
 tempo_médio_uso = float(input("Tempo médio de uso diário (em horas): "))
+
+# Consumo mensal
 consumo_mensal = (potência * tempo_médio_uso * 30) / 1000
+
+# Tarifa de energia elétrica
 tarifa = 0.75
+
+# Custo final
 custo_estimado = consumo_mensal * tarifa
+
+# Resultado
 print(f"O consumo mensal do aparelho {nome} é de: {consumo_mensal:.2f} kWh.")
 print(f"O custo estimado é de: R$ {custo_estimado:.2f}.")
